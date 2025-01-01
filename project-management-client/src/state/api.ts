c0c0f501596e4fb585db6ed28e798de266539b4d
Nguyen Duc Kien <ngdkien12112k3@gmail.com>
2025-01-01 15:02:13 +0700

    Search Tab

--- project-management-client/src/state/api.ts
+++ project-management-client/src/state/api.ts
@@ -119,6 +119,9 @@ export const api = createApi({
                 { type: "Tasks", id: taskId },
             ],
         }),
+        search: build.query<SearchResults, string>({
+            query: (query) => `search?query=${query}`,
+        }),
     }),
 });
 
@@ -128,7 +131,7 @@ export const {
     useGetTasksQuery,
     useCreateTaskMutation,
     useUpdateTaskStatusMutation,
-    //   useSearchQuery,
+    useSearchQuery,
     //   useGetUsersQuery,
     //   useGetTeamsQuery,
     //   useGetTasksByUserQuery,
